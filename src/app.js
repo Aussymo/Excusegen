16949ca89e52cc8c52c9d398483c325f9906481a
Aussymo <aussymo@gmail.com>
2022-04-05 01:30:31 +0000

    Created Repo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,18 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#button").addEventListener("click", () => {
+    document.querySelector("#domain").innerHTML = domainGen();
+  });
+};
+let domainGen = () => {
+  let who = ["bear", "mom", "dad", "wife"];
+  let action = ["chased", "attacked", "ripped", "ate"];
+  let what = ["homework", "project", "test", "report"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+
+  return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]}`;
 };
